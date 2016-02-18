db2823d19f3462744a90619c1771f28d02a0cc96
Antoine <antoine.lafleur@hotmail.com>
2016-02-18 06:31:44 -0500

    Documentation des fonctions effectuées.

--- ColonneCartes.cpp
+++ ColonneCartes.cpp
@@ -8,15 +8,24 @@
 #include "ColonneCartes.h"
 #include "Solitaire.h"
 #include <sstream>
-#include <iostream>
 
 using namespace std;
 
+/**
+ * \brief Constructeur par default. On assigne 1 aux carte visibles.
+ *
+ * \post Une instance de la classe.
+ */
 ColonneCartes::ColonneCartes() : m_nbCartesVisibles(1)
 {
 
 }
 
+/** \fn void initColonneCartes(vector<Carte>& p_listeCartes)
+ * \brief Initialisation de la colonne de carte. On y assigne les cartes.
+ *
+ * \param p_listeCartes La liste que devra contenir la colonne.
+ */
 void ColonneCartes::initColonneCartes(vector<Carte>& p_listeCartes)
 {
 	vector<Carte>::iterator iter;
@@ -27,6 +36,11 @@ void ColonneCartes::initColonneCartes(vector<Carte>& p_listeCartes)
 	}
 }
 
+/** \fn void ajoute(const Carte& p_carte)
+ * \brief Fonction permettant d'ajouter une carte à la colonne.
+ *
+ * \param p_carte Carte à ajouter au bout de la colonne.
+ */
 void ColonneCartes::ajoute(const Carte& p_carte) {
 	//Si valeur p_carte est égal à la valeur de m_carte.end() -1 et que la sorte est de couleur différente:
 	//Je pousse la carte.
@@ -56,6 +70,13 @@ void ColonneCartes::ajoute(const Carte& p_carte) {
 		}
 	}
 }
+
+/** \fn void deplacePaquet(colonneCartes& p_destination, int p_nombreCartes)
+ * \brief Fonction permettant de déplacer une ou plusieurs cartes entre deux colonnes différentes.
+ *
+ * \param p_destination Colonne ou la ou les cartes doivent être ajoutées.
+ * \param p_nombreCartes Le nombre de cartes que l'utilisateurs veut déplacer.
+ */
 void ColonneCartes::deplacePaquet(ColonneCartes& p_destination, int p_nombreCartes)
 {
 	vector<Carte> paquetADeplacer;
@@ -89,6 +110,11 @@ void ColonneCartes::deplacePaquet(ColonneCartes& p_destination, int p_nombreCart
 	}
 }
 
+/** \fn void supprimeDerniereCarte();
+ * \brief Fonction qui supprime la carte située à la fin de la colonne. Si la carte supprimée
+ * rend m_nbCartesVisibles à 0 et que la colonne n'est pas vide. On ajoute 1 pour afficher la dernière carte.
+ *
+ */
 void ColonneCartes::supprimeDerniereCarte()
 {
 	m_lesCartes.pop_back();
@@ -98,21 +124,42 @@ void ColonneCartes::supprimeDerniereCarte()
 		m_nbCartesVisibles++;
 	}
 }
+
+/** \fn int reqNbCartesVisibles()
+ * \brief Fonction retournant la valeur membre m_nbCartesVisibles.
+ *
+ * \post La variable membre m_nbCartes Visible est retournée.
+ */
 int ColonneCartes::reqNbCartesVisibles() const
 {
 	return m_nbCartesVisibles;
 }
 
+/** \fn Carte derniereCarteColonne()
+ * \brief Fonction retournant la dernière carte de la colonne.
+ *
+ * \post La dernière carte de la colonne est retournée.
+ */
 Carte ColonneCartes::derniereCarteColonne()
 {
 	return m_lesCartes.back();
 }
 
+/** \fn int colonneEstVide()
+ * \brief Fonction permettant de connaître la taille de la colonne.
+ *
+ * \post La taille de la colonne est retournée.
+ */
 int ColonneCartes::colonneEstVide() const
 {
 	return m_lesCartes.size();
 }
 
+/** \fn std::string sortieFormate()
+ * \brief Fonction qui formatte les informations de la colonne dans une string.
+ *
+ * \post Retourne une string avec toutes les informations requises de la colonne.
+ */
 std::string ColonneCartes::sortieFormate() const {
 	std::string colonne;
 	if(!this->m_lesCartes.empty())
@@ -136,6 +183,11 @@ std::string ColonneCartes::sortieFormate() const {
 	return colonne;
 }
 
+/**
+ * \brief Operateur<< surchargé permettant d'afficher toute la colonne retournée par la fonction sortieFormate().
+ *
+ * \post un ostream contenant toutes les informations de la colonne.
+ */
 std::ostream& operator<<(std::ostream & os, const ColonneCartes& p_colonneCartes)
 {
 	std::string sortie;
